fe14a52e69b94ea9f22d7c7cd83257b639f8e247
Anointed Emecheta <annyemecheta98@gmail.com>
2023-11-27 09:52:36 +0100

    final commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const apiKey = "92086593af7b8ab58e90c6a3ce90d2b4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "/WEATHER APPLICATION USING HTML,CSS AND JAVASCRIPT/images/cloudy (1).png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "/WEATHER APPLICATION USING HTML,CSS AND JAVASCRIPT/images/sunny.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "/WEATHER APPLICATION USING HTML,CSS AND JAVASCRIPT/images/rainy-day.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "/WEATHER APPLICATION USING HTML,CSS AND JAVASCRIPT/images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "/WEATHER APPLICATION USING HTML,CSS AND JAVASCRIPT/images/haze.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    }
+    
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
